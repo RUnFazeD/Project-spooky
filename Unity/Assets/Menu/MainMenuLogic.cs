89e979c0cbc55c159541d407655cbc4362ccfe61
RUnFazeD <78729048+RUnFazeD@users.noreply.github.com>
2023-01-14 05:33:52 +0100

    Intro and all my assets

--- Unity/Assets/Menu/MainMenuLogic.cs
+++ Unity/Assets/Menu/MainMenuLogic.cs
@@ -8,22 +8,46 @@ public class MainMenuLogic : MonoBehaviour
     public GameObject mainMenu;
     public GameObject extrasMenu;
     public GameObject loading;
+    public GameObject Intro;
+    public GameObject Video;
+    public GameObject MainText;
+    public GameObject buttons;
+    public GameObject mainmenumusic;
+
+    public float waitTime;
 
     public AudioSource buttonSound;
-
+    public AudioSource TapeInsert;
 
 
     void Start()
     {
-
+        Intro.SetActive(true);
+        MainText.SetActive(false);
+        TapeInsert.Play();
+        mainmenumusic.SetActive(false);
+        Video.SetActive(false);
+        buttons.SetActive(false);
         mainMenu.GetComponent<Canvas>().enabled = true;
         extrasMenu.GetComponent<Canvas>().enabled = false; 
-        loading.GetComponent<Canvas>().enabled = false; 
+        loading.GetComponent<Canvas>().enabled = false;
+        StartCoroutine(Starting());
+    }
+    
+    IEnumerator Starting()
+    {
+        yield return new WaitForSeconds(waitTime);
+        Intro.SetActive(false);
+        Video.SetActive(true);
+        MainText.SetActive(true);
+        buttons.SetActive(true);
+        mainmenumusic.SetActive(true);
     }
 
     public void StartButton()
     {
         loading.GetComponent<Canvas>().enabled = true;
+        Intro.SetActive(true);
         mainMenu.GetComponent<Canvas>().enabled = false;
         buttonSound.Play();
         SceneManager.LoadScene("Royscriptprototype");
