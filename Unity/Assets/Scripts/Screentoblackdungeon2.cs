9d352b14e5ab04fdab796c1e563f237ca08128cd
RUnFazeD <78729048+RUnFazeD@users.noreply.github.com>
2023-01-16 05:20:07 +0100

    big finish

---
+++ Unity/Assets/Scripts/Screentoblackdungeon2.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Screentoblackdungeon2 : MonoBehaviour
+{
+    public GameObject blkscreen;
+
+    public float waitTime;
+
+
+    void Start()
+    {
+        blkscreen.SetActive(false);
+    }
+
+
+    void Update()
+    {
+        if (blkscreen.activeInHierarchy)
+        {
+            StartCoroutine(Starting());
+        }
+
+    }
+    IEnumerator Starting()
+    {
+        yield return new WaitForSeconds(waitTime);
+        SceneManager.LoadScene("Dungeon Map 2");
+
+    }
+}
